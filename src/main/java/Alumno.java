aabbb03ad3c65dc24985aa1a82ea34a3ec9a0cd8
carlosgomezchico <145378795+carlosgomezchico@users.noreply.github.com>
2024-01-21 12:39:19 +0000

    Update Alumno.java

--- src/main/java/Alumno.java
+++ src/main/java/Alumno.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Alumno {
+package PACKAGE_NAME;
+  
+public class Alumno {
+    private String nombre;
+    private int edad;
+
+    public Alumno(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" +
+               "nombre='" + nombre + '\'' +
+               ", edad=" + edad +
+               '}';
+    }
 }
